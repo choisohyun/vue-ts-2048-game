31ae3bdfbe6f278024d4cee18cc8cbbb257751bf
choisohyun <sohyun4283@naver.com>
2020-11-03 17:47:10 +0900

    Add ScoreContainer props and style

--- src/components/Container/ScoreContainer.vue
+++ src/components/Container/ScoreContainer.vue
@@ -1,14 +1,33 @@
 <template>
   <div class="container">
-    <div class="score-title">{{}}</div>
-    <div class="score">{{}}</div>
+    <div class="score-title">{{ title }}</div>
+    <div class="score">{{ score }}</div>
   </div>
 </template>
 
-<script>
-import { Vue, Prop } from "vue-property-decorator";
+<script lang="ts">
+import { Prop, Component } from "vue-property-decorator";
+import Vue from "vue";
 
-export default class ScoreContainer extends Vue {}
+@Component
+export default class ScoreContainer extends Vue {
+  @Prop({ default: "" }) title!: string;
+  @Prop({ default: 0 }) score!: number;
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  background-color: #ffe4c4;
+  margin: 0.5rem;
+  padding: 0.5rem 1rem;
+  border-radius: 1rem;
+  font-weight: bold;
+}
+.score-title {
+  font-size: 1.8rem;
+}
+.score {
+  font-size: 1.6rem;
+}
+</style>
